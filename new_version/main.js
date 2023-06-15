b9fc7abd85353acd614d523d1c8a0de2cb72ca1c
TheKennyman07 <69918528+KennyMan14@users.noreply.github.com>
2023-06-15 14:54:30 -0600

    add the new files for the game

---
+++ new_version/main.js
@@ -0,0 +1,30 @@
+import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.mjs";
+
+kaboom({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  crisp: true,
+  // canvas: document.getElementById('kaboom'),
+  background: [0,0,0],
+  global: true,
+})
+
+loadSprite("construction", "../assets/sprites/construction_ahead.png");
+loadFont("sink", "../assets/kitchen_sink.ttf");
+
+scene("default", () => {
+  add([
+    sprite("construction"),
+    anchor("center"),
+    pos(width()/2, height()/3),
+    scale(2),
+  ])
+  
+  add([
+    text("Ooops! We're cleaning some stuff over \nhere... wait a bit!", {font: "sink", align: "center"}),
+    anchor("center"),
+    pos(width()/2, height()/1.5),
+  ]);
+})
+
+go('default');
